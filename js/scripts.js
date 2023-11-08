511c5304e746141c0b75912a2cf7d8b3007c9e6e
Kirsten Smith <kirstenalexis7@gmail.com>
2023-11-08 09:39:36 -0800

    add tests 4 & 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 // Business Logic
 
 function wordCounter(text) {
-  if (text.length === 0) {
+  if (text.trim().length === 0) {
     return 0;
   }
   let wordCount = 0;
   const textArray = text.split(" ");
-  textArray.forEach(function (word) {
-    wordCount++;
+  textArray.forEach(function (element) {
+    if (!Number(element))
+      wordCount++;
   });
   return wordCount;
 }
