6bc1d82e325127763599a0f5ab9cad7c7988f8b5
Kirsten Smith <kirstenalexis7@gmail.com>
2023-11-08 11:29:40 -0800

    add new test function

--- README.md
+++ README.md
@@ -55,3 +55,17 @@ const text = "red blue red red red green";
 const word = "red";
 numberOfOccurrencesInText(word, text);
 Expected Output: 4
+
+Test: "It should return a word match regardless of case."
+Code:
+const text = "red RED Red green Green GREEN";
+const word = "Red";
+numberOfOccurrencesInText(word, text);
+Expected Output: 3
+
+Test: "It should return a word match regardless of punctuation."
+Code:
+const text = "Red! Red. I like red, green, and yellow.";
+const word = "Red";
+numberOfOccurrencesInText(word, text);
+Expected Output: 3
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Business Logic
 
-/*function wordCounter(text) {
+unction wordCounter(text) {
   if (text.trim().length === 0) {
     return 0;
   }
@@ -12,15 +12,35 @@
   });
   return wordCount;
 }
-*/
+
 
 function numberOfOccurrencesInText(word, text) {
   const textArray = text.split(" ");
   let wordCount = 0;
   textArray.forEach(function (element) {
-    if (word === element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
       wordCount++
     }
   });
   return wordCount
 }
+
+function naughtyWord(word, text) {
+  const naughty = ["zoinks", "muppeteer", "biffaroni", "loopdaloop"];
+  const textArray = text.split(" ");
+  let cleanText = "";
+
+  textArray.forEach(function (element) {
+    const lowerElement = element.toLowerCase();
+    if (!naughty.some(naughtyWord => lowerElement.includes(naughtyWord.toLowerCase()))) {
+      cleanText += element + " ";
+    }
+  });
+
+  return cleanText.trim();
+}
+
+const inputText = "This is a zoinks test. Please remove muppeteer naughty words.";
+const cleanedText = naughtyWord("naughty", inputText);
+
+console.log(cleanedText);
